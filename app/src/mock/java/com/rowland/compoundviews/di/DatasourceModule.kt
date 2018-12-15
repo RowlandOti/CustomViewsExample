db28dc17d3ed59e13fa9c63325cdeaa2684a0dcc
RowlandOti <rowlandotienoo@gmail.com>
2018-12-15 20:39:40 +0300

    feat: how to test a custom view

---
+++ app/src/mock/java/com/rowland/compoundviews/di/DatasourceModule.kt
@@ -0,0 +1,16 @@
+package com.rowland.compoundviews.di
+
+import com.rowland.compoundviews.contract.IAccountDataSource
+import com.rowland.compoundviews.data.MockAccountDataSource
+import dagger.Module
+import dagger.Provides
+
+/**
+ * Created by Rowland on 12/15/2018.
+ */
+
+@Module
+class DataSourceModule {
+    @Provides
+    fun providesMockAccountDataSource(): IAccountDataSource = MockAccountDataSource()
+}
