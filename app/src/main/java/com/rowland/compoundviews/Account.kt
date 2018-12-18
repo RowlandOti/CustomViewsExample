3b8ac434f59e070efbbf166322e581e166d166bf
RowlandOti <rowlandotienoo@gmail.com>
2018-12-18 06:26:38 +0300

    chore: remove unused code

--- app/src/main/java/com/rowland/compoundviews/Account.kt
+++
@@ -1,9 +0,0 @@
-package com.rowland.compoundviews
-
-/**
- * Created by Rowland on 12/13/2018.
- */
-sealed class Account {
-    object LoggedOut: Account()
-    data class Active(val name: String, val avatar: Int, val activated: String): Account()
-}
